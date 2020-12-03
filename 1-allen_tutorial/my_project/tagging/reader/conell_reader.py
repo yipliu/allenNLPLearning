a0c81e869953d3bb67fb58a5be93527987801159
yipliu <497603618@qq.com>
2020-12-03 22:47:25 +0800

    construct directory structure

---
+++ 1-allen_tutorial/my_project/tagging/reader/conell_reader.py
@@ -0,0 +1,8 @@
+from typing import Dict, Iterable, List
+
+from allennlp.data.dataset_readers.dataset_reader import DatasetReader
+from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenIndexer
+
+@DatasetReader.register("conll_03_reader")
+class CoNLL03DatasetReader(DatasetReader):
+    pass
