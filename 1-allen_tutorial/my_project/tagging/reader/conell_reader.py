d1ae64289f6fb7dbcb01222c3bdd8347ccd15d48
yipliu <497603618@qq.com>
2020-12-04 09:16:17 +0800

    init

--- 1-allen_tutorial/my_project/tagging/reader/conell_reader.py
+++ 1-allen_tutorial/my_project/tagging/reader/conell_reader.py
@@ -6,3 +6,5 @@ from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenIndexer
 @DatasetReader.register("conll_03_reader")
 class CoNLL03DatasetReader(DatasetReader):
     pass
+
+clas no
